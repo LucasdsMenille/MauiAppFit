4c186fb8213acb47df0a57eaea0c0e57612f3631
231053 <231053@CPD02-M05>
2025-07-28 11:39:38 -0300

    atualizacao 2

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
-us
+using MauiAppFit.Models;
 
 namespace MauiAppFit.Helpers
 {
@@ -12,10 +12,41 @@ namespace MauiAppFit.Helpers
     {
         readonly SQLiteAsyncConnection _db;
 
-        public SQLiteDataBaseHelper(string dbPath) 
-        { 
+        public SQLiteDataBaseHelper(string dbPath)
+        {
             _db = new SQLiteAsyncConnection(dbPath);
             _db.CreateTableAsync<Atividade>().Wait();
+        }
+
+        public Task<List<Atividade>> GetAllRows()
+        {
+            return _db.Table<Atividade>().OrderByDescending(i => i.Id).ToListAsync();
+        }
+
+        public Task<Atividade> GetByID(int id)
+        {
+            return _db.Table<Atividade>().FirstAsync(i => i.Id == id);
+        }
+
+        public Task<int> Insert(Atividade model)
+        {
+            return _db.InsertAsync(model);
+        }
+
+        public Task<List<Atividade>> Update(Atividade model)
+        {
+            string sql = "UPDATE Atividade SET Descricao=?, Data=?, Peso=?, " + "Observacoes=? WHERE Id=?";
+
+            return _db.QueryAsync<Atividade>
+       (
+          sql,
+          model.Descricao,
+          model.Data,
+          model.Peso,
+          model.Observacoes,
+          model.Id
+       );
+
         }
     }
 }
