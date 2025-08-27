c76632f0167b4e3f50e2b552c8c79114ca411752
231053 <231053@CPD02-M05>
2025-08-27 14:43:07 -0300

    ListaAtividadesViewModels

--- MauiAppFit/ViewModels/ListaAtividadesViewModel.cs
+++ MauiAppFit/ViewModels/ListaAtividadesViewModel.cs
@@ -1,12 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppFit.Models;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MauiAppFit.ViewModels
 {
-    internal class ListaAtividadesViewModel
+    internal class ListaAtividadesViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /**
+         * pegando o que foi digitado pelo usuário.
+         */
+
+        public string ParametroBusca { get; set; }
+
+        /** 
+         * Gerencia se mostra ao usuário o RefreshView.
+        */
+        bool estaAtualizando = false;
+
+        public bool EstaAtualizando
+        {
+            get => estaAtualizando;
+            set
+            {
+                estaAtualizando = value;
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs("EstaAtualizando"));
+            }
+        }
+                /** 
+                     * Coleção que armazena as atividades do usuário.
+                */
+                ObservableCollection<Atividade> listaAtividades = new ObservableCollection<Atividade>();
+                public ObservableCollection<Atividade> ListaAtividades
+                { 
+                    get => listaAtividades;
+                    set => listaAtividades = value;
+                }
+        public ICommand AtualizarLista
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    try
+                    {
+                        if (EstaAtualizando)
+                            return;
+                        EstaAtualizando = true;
+
+                        List<Atividade> tmp =
+                        await App.Database.GetAllRows();
+
+                        ListaAtividades.Clear();
+                        tmp.ForEach(i => ListaAtividades.Add(i));
+                    }
+                    catch (Exception ex)
+                    {
+                        await Shell.Current.DisplayAlert(
+                            "Ops", ex.Message, "OK");
+                    }
+                    finally
+                    {
+                        EstaAtualizando = false;
+                    }
+
+                });
+            } //Fecha get
+        } //Fecha AtualizarLista
     }
 }
